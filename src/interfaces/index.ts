7e2e9a190a479105217e8cc7d056a0f17e717d88
Crystal P <sj8871@gmail.com>
2024-06-18 22:51:51 +0900

    fix : login interface memberId -> user 필드 수정 login 파일 분리

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,18 +2,17 @@ import mongoose from 'mongoose';
 
 export * from './content';
 export * from './result';
+export * from './login';
 
 export interface IBase extends Document {
   contentId: mongoose.Types.ObjectId;
-  memberId: mongoose.Types.ObjectId;
+  userId: mongoose.Types.ObjectId;
   createdAt: number;
 }
 
 export interface IShare extends IBase {
   type: 'Kakao' | 'Link';
 }
-export interface IMemberResult extends IBase {
-  menubarId: mongoose.Types.ObjectId;
+export interface IUserResult extends IBase {
+  resultId: mongoose.Types.ObjectId;
 }
-
-export interface ILoginHistory extends Omit<IBase, 'contentId'> {}
