a967d8b1b0516e33b27e14ec08ff329aeb317707
Crystal P <sj8871@gmail.com>
2024-06-19 02:17:48 +0900

    chore: modify eslintrc rule and gitignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,21 +2,24 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   root: true,
+  env: {
+    node: true,
+  },
   rules: {
     semi: 'off',
     'arrow-body-style': ['error', 'as-needed'],
     indent: 'off',
     quotes: 'off',
     'comma-dangle': 'off',
-    // 'no-console': 'warn',
+    'no-console': 'off',
     'no-debugger': 'error',
-    'no-trailing-spaces': 'error',
+    'no-trailing-spaces': 'warn',
     'jsx-quotes': ['error', 'prefer-double'],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error',
+      'warn',
       {
         args: 'all',
         argsIgnorePattern: '^_',
