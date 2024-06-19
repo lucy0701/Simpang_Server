81aae5343229db9b3ceab47b0da48bba23f94f71
Crystal P <sj8871@gmail.com>
2024-06-19 19:05:59 +0900

    feat: add upload - ImgeBB 클라우드에 파일 업로드 로직 추가 - 이미지 파일 웹 등록 후, 해당 이미지의 url을 반환받음

---
+++ src/utils/upload.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { IMAGE_BB_API_KEY } from '../constants';
+import FormData from 'form-data';
+
+export const uploadToImageBB = async (imageFile: Express.Multer.File) => {
+  const data = new FormData();
+  data.append('image', imageFile.buffer.toString('base64'));
+  data.append('key', IMAGE_BB_API_KEY);
+
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api.imgbb.com/1/upload',
+    headers: {
+      ...data.getHeaders(),
+    },
+    data: data,
+  };
+
+  try {
+    const response = await axios.request(config);
+    return response.data.data.url;
+  } catch (error) {
+    throw new Error('Image upload failed');
+  }
+};
