6be69496c0c123026ca5639bf1a0219e91596ffe
Crystal P <sj8871@gmail.com>
2024-06-25 14:45:41 +0900

    refactor: add validate pagination middleware - page params 검증 미들웨어 추가

---
+++ src/middlewares/validatePagination.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const validatePagination = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { size, page, sort } = req.query;
+    if (!size || !size) {
+      return res.status(400).json({ message: 'Page size and page number are required.' });
+    }
+
+    if (Number(size) < 1 || Number(page) < 1) {
+      return res.status(400).json({ message: 'Invalid page number or page size. Both must be greater than 0' });
+    }
+
+    req.query = { size, page, sort };
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
