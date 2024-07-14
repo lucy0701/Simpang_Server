892368ab8a6b914e0b64266160128ace06382370
Crystal P <sj8871@gmail.com>
2024-07-14 17:30:42 +0900

    fix: kakao login redirect_uri 코드 수정 - FE_URL : 로컬환경, 서비스웹 모두 적용 가능하게 url을 쿼리로 받도록 변경

--- src/routes/kakao.ts
+++ src/routes/kakao.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import express, { Request, Response, NextFunction } from 'express';
 
-import { FE_URL, REST_API_KEY, STATUS_MESSAGES } from '../constants';
+import { REST_API_KEY, STATUS_MESSAGES } from '../constants';
 import { IUser } from '../interfaces';
 import { AuthToken, UserInfoResponse } from '../types';
 import JwtService from '../utils/jwtService';
@@ -12,80 +12,84 @@ import UserModel from '../schemas/User';
 
 const router = express.Router();
 
-router.get('/login', async (req: Request<{}, {}, {}, { code: string }>, res: Response, next: NextFunction) => {
-  // #swagger.tags = ['Kakao OAuth']
-  try {
-    const code = req.query.code;
+router.get(
+  '/login',
+  async (req: Request<{}, {}, {}, { code: string; url: string }>, res: Response, next: NextFunction) => {
+    // #swagger.tags = ['Kakao OAuth']
+    try {
+      const code = req.query.code;
+      const url = req.query.url;
 
-    if (!code) {
-      return res.status(400).json({ message: STATUS_MESSAGES.PROVIDE_ACCESS_TOKEN });
-    }
+      if (!code) {
+        return res.status(400).json({ message: STATUS_MESSAGES.PROVIDE_ACCESS_TOKEN });
+      }
 
-    const authToken = await axios.post<AuthToken>(
-      'https://kauth.kakao.com/oauth/token',
-      {},
-      {
-        headers: { 'Content-type': 'application/x-www-form-urlencoded' },
-        params: {
-          grant_type: 'authorization_code',
-          client_id: REST_API_KEY,
-          redirect_uri: FE_URL + '/login/kakao',
-          code,
+      const authToken = await axios.post<AuthToken>(
+        'https://kauth.kakao.com/oauth/token',
+        {},
+        {
+          headers: { 'Content-type': 'application/x-www-form-urlencoded' },
+          params: {
+            grant_type: 'authorization_code',
+            client_id: REST_API_KEY,
+            redirect_uri: url,
+            code,
+          },
         },
-      },
-    );
+      );
 
-    const accessToken = authToken.data.access_token;
+      const accessToken = authToken.data.access_token;
 
-    const userInfoResponse = await axios.post<UserInfoResponse>(
-      'https://kapi.kakao.com/v2/user/me',
-      {},
-      {
-        headers: {
-          'Content-type': 'application/x-www-form-urlencoded',
-          Authorization: `Bearer ${accessToken}`,
+      const userInfoResponse = await axios.post<UserInfoResponse>(
+        'https://kapi.kakao.com/v2/user/me',
+        {},
+        {
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Bearer ${accessToken}`,
+          },
         },
-      },
-    );
+      );
 
-    const userInfoData = userInfoResponse.data;
+      const userInfoData = userInfoResponse.data;
 
-    let user = await UserModel.findOne<IUser>({ kakaoId: userInfoData.id });
+      let user = await UserModel.findOne<IUser>({ kakaoId: userInfoData.id });
 
-    if (user) {
-      user.name = userInfoData.kakao_account.profile.nickname;
-      user.thumbnail = userInfoData.kakao_account.profile.thumbnail_image_url;
-      await user.save();
-    } else {
-      user = await UserModel.create({
-        kakaoId: userInfoData.id,
-        name: userInfoData.kakao_account.profile.nickname,
-        thumbnail: userInfoData.kakao_account.profile.thumbnail_image_url,
+      if (user) {
+        user.name = userInfoData.kakao_account.profile.nickname;
+        user.thumbnail = userInfoData.kakao_account.profile.thumbnail_image_url;
+        await user.save();
+      } else {
+        user = await UserModel.create({
+          kakaoId: userInfoData.id,
+          name: userInfoData.kakao_account.profile.nickname,
+          thumbnail: userInfoData.kakao_account.profile.thumbnail_image_url,
+        });
+      }
+
+      await LoginModel.create({
+        userId: user._id,
       });
+
+      const userInfo: Pick<IUser, 'name' | 'thumbnail' | 'createdAt'> = {
+        name: user.name,
+        thumbnail: user.thumbnail,
+        createdAt: user.createdAt,
+      };
+
+      const token = JwtService.createJWT(user, accessToken);
+
+      res.setHeader('Authorization', `Bearer ${token}`);
+      res.status(200).json(userInfo);
+    } catch (error) {
+      console.error('Error during Kakao OAuth process:', error);
+      if (axios.isAxiosError(error)) {
+        console.error('Axios error response data:', error.response?.data);
+      }
+      next(error);
     }
-
-    await LoginModel.create({
-      userId: user._id,
-    });
-
-    const userInfo: Pick<IUser, 'name' | 'thumbnail' | 'createdAt'> = {
-      name: user.name,
-      thumbnail: user.thumbnail,
-      createdAt: user.createdAt,
-    };
-
-    const token = JwtService.createJWT(user, accessToken);
-
-    res.setHeader('Authorization', `Bearer ${token}`);
-    res.status(200).json(userInfo);
-  } catch (error) {
-    console.error('Error during Kakao OAuth process:', error);
-    if (axios.isAxiosError(error)) {
-      console.error('Axios error response data:', error.response?.data);
-    }
-    next(error);
-  }
-});
+  },
+);
 
 router.get('/logout', loginChecker, async (req: Request, res: Response, next: NextFunction) => {
   // #swagger.tags = ['Kakao OAuth']
